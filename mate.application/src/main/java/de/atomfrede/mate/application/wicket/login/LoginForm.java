e8ff27048d06a5d08104c034212bc0089768cd3e
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-31 19:51:02 +0100

    Working login page/form with feedback.

--- mate.application/src/main/java/de/atomfrede/mate/application/wicket/login/LoginForm.java
+++ mate.application/src/main/java/de/atomfrede/mate/application/wicket/login/LoginForm.java
@@ -2,13 +2,13 @@ package de.atomfrede.mate.application.wicket.login;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.feedback.FeedbackMessage;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import de.atomfrede.mate.application.wicket.base.AbstractBaseForm;
@@ -16,6 +16,14 @@ import de.atomfrede.mate.application.wicket.security.UserAuthModel;
 import de.atomfrede.mate.domain.entities.user.User;
 import de.atomfrede.mate.service.user.UserService;
 
+/**
+ * A form component that provides login functionality for users.
+ * 
+ * It also handles errors nicely with appropriate feedback for the user.
+ * 
+ * @author fred
+ * 
+ */
 public class LoginForm extends AbstractBaseForm<User> {
 
 	@SpringBean
@@ -25,85 +33,80 @@ public class LoginForm extends AbstractBaseForm<User> {
 	PasswordTextField passwordTextField;
 	FeedbackPanel feedbackPanel;
 	WebMarkupContainer usernameContainer, passwordContainer;
-	
-	
-	
+
 	public LoginForm(String id, UserAuthModel model) {
 		super(id, new CompoundPropertyModel<User>(model));
-		
+
 		feedbackPanel = new FeedbackPanel("feedbackPanel");
 		add(feedbackPanel);
 		feedbackPanel.setVisible(false);
-		
+
 		usernameContainer = new WebMarkupContainer("username_container");
 		feedbackPanel.add(usernameContainer);
 		add(usernameContainer);
-		
+
 		usernameTextField = new RequiredTextField<String>("username");
 		usernameContainer.add(usernameTextField);
-		
+
 		passwordContainer = new WebMarkupContainer("password_container");
 		feedbackPanel.add(passwordContainer);
 		add(passwordContainer);
-		
+
 		passwordTextField = new PasswordTextField("password");
 		passwordContainer.add(passwordTextField);
 	}
 
-	protected void setFeedbackPanel(FeedbackPanel feedback){
+	protected void setFeedbackPanel(FeedbackPanel feedback) {
 		this.feedbackPanel = feedback;
 	}
-	
-	protected void onBeforeRender(){
-		if(!hasError()){
+
+	@Override
+	protected void onBeforeRender() {
+		if (!hasError()) {
+			// If the form is reloaded via F5 or so we should remove all stuff
+			// indicating an error if no error is present anymore
 			this.feedbackPanel.setVisible(false);
-			this.usernameContainer.add(new AttributeModifier("class", "control-group"));
-			this.passwordContainer.add(new AttributeModifier("class", "control-group"));
+			this.usernameContainer.add(new AttributeModifier("class",
+					"control-group"));
+			this.passwordContainer.add(new AttributeModifier("class",
+					"control-group"));
 		}
 		super.onBeforeRender();
 	}
+
 	@Override
-	protected void onError(){
-		System.out.println("OnError!");
+	protected void onError() {
+		// Only on validation errors we make the feedbackpanel visible
 		this.feedbackPanel.setVisible(true);
-		
-		this.usernameContainer.add(new AttributeAppender("class", " warning"));
-		this.passwordContainer.add(new AttributeAppender("class", " warning"));
+		this.usernameContainer.add(new AttributeAppender("class", " error"));
+		this.passwordContainer.add(new AttributeAppender("class", " error"));
 	}
-	
+
 	@Override
 	public void onSubmit() {
 		String username = getModelObject().getUsername();
-		String email = getModelObject().getPassword();
+		String password = getModelObject().getPassword();
 
-		User user = userService.getByUsername(email);
-		
-		//Check first if the user is not null
-		if(user == null){
-			//Put a generic error message like user not found or password incorrect
+		User user = userService.getByUsername(username);
+
+		// Check first if the user is not null
+		if (user == null) {
+			// Put a generic error message like user not found or password
+			// incorrect
+			error("Benutzer nicht gefunden oder Password nicht korrekt.");
 			return;
-		}else {
-			//Check if the passwort is correct for the user found by the provided username
+		} else {
+			// Check if the password is correct for the user found by the
+			// provided username
+			if(!user.isPassword(password)){
+				return;
+			}
 		}
-		//
-		Object ses = getSession();
 		getSession().setUser(new UserAuthModel(User.class, user.getId()));
-		
+
 		setResponsePage(getApp().getHomePage());
 		return;
-		
-		// String userName = getModelObject().getUsername();
-		// String userName = getModelObject().getObject().getUsername();
-		// String password = getModelObject().getObject().getPassword();
 
-		// User user = userService.getByUserName(userName);
-		//
-		// if(user != null){
-		//
-		// }
-
-		// getModelObject().getObject()
-		// UserMode user =
 	}
 
 }
--- mate.application/src/main/java/de/atomfrede/mate/application/wicket/login/LoginPage.java
+++ mate.application/src/main/java/de/atomfrede/mate/application/wicket/login/LoginPage.java
@@ -7,6 +7,7 @@ import de.atomfrede.mate.application.wicket.base.AbstractBasePage;
 import de.atomfrede.mate.application.wicket.security.UserAuthModel;
 import de.atomfrede.mate.domain.entities.user.User;
 import de.atomfrede.mate.service.user.UserService;
+import de.atomfrede.mate.service.user.UsernameAlreadyTakenException;
 
 @MountPath(value = "/", alt = "/login")
 public class LoginPage extends AbstractBasePage {
@@ -15,8 +16,18 @@ public class LoginPage extends AbstractBasePage {
 	public UserService userService;
 
 	public LoginPage() {
+		addDummyUser();
 		UserAuthModel userModel = new UserAuthModel(User.class, -1L);
 		add(new LoginPanel("loginPanel", userModel));
+	}
+	
+	private void addDummyUser(){
+		try {
+			userService.createUser("fred", "Frederik", "Hahne", "fred@mail.de", "fred");
+		} catch (UsernameAlreadyTakenException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
