62f6e55b5ba760ffa439ac0256f6d72da06ffd69
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-31 19:49:34 +0100

    BAse Entity Service with type parameter

---
+++ mate.service/src/main/java/de/atomfrede/mate/service/EntityService.java
@@ -0,0 +1,23 @@
+package de.atomfrede.mate.service;
+
+import java.util.List;
+
+import de.atomfrede.mate.domain.entities.IEntity;
+
+public interface EntityService<EntityClass extends IEntity> {
+
+	List<EntityClass> list(long offset, long count);
+
+	List<EntityClass> findAll();
+
+	EntityClass findById(Long id);
+
+	EntityClass findByProperty(String propertyName, Object propertyValue);
+
+	void remove(EntityClass entity);
+
+	void persist(EntityClass entity);
+
+	long count();
+
+}
