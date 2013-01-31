7f599e822166d795f6456a39e9a35599daf7e2bd
Frederik Hahne <atomfrede@gmail.com>
2013-01-31 15:49:57 +0100

    Dao Loaders and Service Loader for Abstract Wicket Models.

--- mate.domain/src/main/java/de/atomfrede/mate/domain/EntityLoaderDao.java
+++ mate.domain/src/main/java/de/atomfrede/mate/domain/EntityLoaderDao.java
@@ -2,7 +2,6 @@ package de.atomfrede.mate.domain;
 
 import java.io.Serializable;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
--- mate.domain/src/main/java/de/atomfrede/mate/domain/entities/user/User.java
+++ mate.domain/src/main/java/de/atomfrede/mate/domain/entities/user/User.java
@@ -19,8 +19,6 @@ public class User extends AbstractEntity {
 
 	private static final long serialVersionUID = -8695856794737512171L;
 
-	private static final long serialVersionUID = -8695856794737512171L;
-
 	@GenericGenerator(name = "UserIdGenerator", strategy = "org.hibernate.id.MultipleHiLoPerTableGenerator", parameters = {
 			@Parameter(name = "table", value = "IdentityGenerator"),
 			@Parameter(name = "primary_key_column", value = "sequence_name"),
