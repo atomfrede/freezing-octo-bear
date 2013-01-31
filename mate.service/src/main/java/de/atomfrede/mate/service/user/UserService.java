9f76a344bec4f14d84dfe885af98a64624badaf2
Frederik Hahne <frederik.hahne@googlemail.com>
2013-01-31 19:50:18 +0100

    Working Userservice.

--- mate.service/src/main/java/de/atomfrede/mate/service/user/UserService.java
+++ mate.service/src/main/java/de/atomfrede/mate/service/user/UserService.java
@@ -1,8 +1,21 @@
 package de.atomfrede.mate.service.user;
 
 import de.atomfrede.mate.domain.entities.user.User;
+import de.atomfrede.mate.service.EntityService;
 
-public interface UserService {
+public interface UserService extends EntityService<User>{
 
 	public User getByUsername(String username);
+
+	/**
+	 * Creates a new user and tries to write it into database.
+	 * 
+	 * @param username
+	 * @param firstname
+	 * @param lastname
+	 * @param email
+	 * @param password
+	 * @return
+	 */
+	public User createUser(String username, String firstname, String lastname, String email, String password) throws UsernameAlreadyTakenException;
 }
--- mate.service/src/main/java/de/atomfrede/mate/service/user/UserServiceImpl.java
+++ mate.service/src/main/java/de/atomfrede/mate/service/user/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package de.atomfrede.mate.service.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,7 +11,7 @@ import de.atomfrede.mate.domain.entities.user.User;
 
 @Service(value = "userService")
 @Transactional(rollbackFor = Exception.class)
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserDao userDao;
@@ -18,6 +20,60 @@ public class UserServiceImpl implements UserService{
 	public User getByUsername(String username) {
 		return userDao.getByUserName(username);
 	}
-	
+
+	@Override
+	public List<User> list(long offset, long count) {
+		return userDao.list(offset, count);
+	}
+
+	@Override
+	public List<User> findAll() {
+		return userDao.findAll();
+	}
+
+	@Override
+	public User findById(Long id) {
+		return userDao.findById(id);
+	}
+
+	@Override
+	public User findByProperty(String propertyName, Object propertyValue) {
+		return userDao.findByProperty(propertyName, propertyValue);
+	}
+
+	@Override
+	public void remove(User entity) {
+		userDao.remove(entity);
+
+	}
+
+	@Override
+	public void persist(User entity) {
+		userDao.persist(entity);
+
+	}
+
+	@Override
+	public long count() {
+		return userDao.count();
+	}
+
+	@Override
+	public User createUser(String username, String firstname, String lastname,
+			String email, String password) throws UsernameAlreadyTakenException {
+		User possibleUser = userDao.findByProperty("username", username);
+		if (possibleUser != null) {
+			throw new UsernameAlreadyTakenException();
+		}
+		User user = new User();
+		user.setUsername(username);
+		user.setFirstname(firstname);
+		user.setLastname(lastname);
+		user.setPassword(password);
+		user.setEmail(email);
+
+		userDao.persist(user);
+		return user;
+	}
 
 }
