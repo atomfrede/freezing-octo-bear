fc954ac3936feb7e29bf592d2abda6f89e7c676b
Frederik Hahne <atomfrede@gmail.com>
2013-02-01 12:15:27 +0100

    Logout page that invalidates the session (though is seems not to be enough.).

---
+++ mate.application/src/main/java/de/atomfrede/mate/application/wicket/logout/LogoutPage.java
@@ -0,0 +1,30 @@
+package de.atomfrede.mate.application.wicket.logout;
+
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.wicketstuff.annotation.mount.MountPath;
+
+import de.atomfrede.mate.application.wicket.base.AbstractBasePage;
+import de.atomfrede.mate.application.wicket.login.LoginPanel;
+import de.atomfrede.mate.application.wicket.security.UserAuthModel;
+import de.atomfrede.mate.domain.entities.user.User;
+import de.atomfrede.mate.service.user.UserService;
+
+@MountPath(value = "/", alt = "/login")
+public class LogoutPage extends AbstractBasePage {
+
+	@SpringBean
+	public UserService userService;
+
+	public LogoutPage() {
+		doLogout();
+		UserAuthModel userModel = new UserAuthModel(User.class, -1L);
+		add(new LoginPanel("loginPanel", userModel));
+	}
+	
+	public void doLogout(){
+		getSession().invalidate();
+	}
+	
+	
+
+}
