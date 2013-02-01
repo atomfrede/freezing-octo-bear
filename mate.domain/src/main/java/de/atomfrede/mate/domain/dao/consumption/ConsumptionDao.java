6c65546b0d6320285ee678331a0b8cbb4b0fe0a2
Frederik Hahne <atomfrede@gmail.com>
2013-02-01 10:57:30 +0100

    DAO for sonsuming a bottle of club mate.

---
+++ mate.domain/src/main/java/de/atomfrede/mate/domain/dao/consumption/ConsumptionDao.java
@@ -0,0 +1,8 @@
+package de.atomfrede.mate.domain.dao.consumption;
+
+import de.atomfrede.mate.domain.dao.DAO;
+import de.atomfrede.mate.domain.entities.consumption.Consumption;
+
+public interface ConsumptionDao extends DAO<Consumption>{
+
+}
