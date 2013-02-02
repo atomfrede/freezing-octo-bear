b8de50e7a2c9c90d16966071449e4105dcef49e5
Frederik Hahne <frederik.hahne@googlemail.com>
2013-02-02 07:50:14 +0100

    Registration Page SKeleton.

---
+++ mate.application/src/main/java/de/atomfrede/mate/application/wicket/register/RegisterPage.java
@@ -0,0 +1,10 @@
+package de.atomfrede.mate.application.wicket.register;
+
+import org.wicketstuff.annotation.mount.MountPath;
+
+import de.atomfrede.mate.application.wicket.base.AbstractBasePage;
+
+@MountPath(value = "/", alt = "/register")
+public class RegisterPage extends AbstractBasePage{
+
+}
