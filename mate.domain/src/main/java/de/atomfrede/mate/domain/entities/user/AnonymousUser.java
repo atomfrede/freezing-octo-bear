b5fd27f81ed94d291be33677455d3727f9001b0f
Frederik Hahne <frederik.hahne@googlemail.com>
2013-02-02 13:27:08 +0100

    Extends Anonymous user for easier use with register panel/form.

--- mate.domain/src/main/java/de/atomfrede/mate/domain/entities/user/AnonymousUser.java
+++ mate.domain/src/main/java/de/atomfrede/mate/domain/entities/user/AnonymousUser.java
@@ -2,6 +2,18 @@ package de.atomfrede.mate.domain.entities.user;
 
 public class AnonymousUser extends User {
 
+	protected String password_2;
+	
 	public AnonymousUser(){
 	}
+
+	public String getPassword_2() {
+		return password_2;
+	}
+
+	public void setPassword_2(String password_2) {
+		this.password_2 = password_2;
+	}
+	
+	
 }
