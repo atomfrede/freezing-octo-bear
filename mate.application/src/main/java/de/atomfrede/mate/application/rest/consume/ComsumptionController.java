357b8e5d87de43541c38ac05692f5ff380fba6f2
Frederik Hahne <frederik.hahne@googlemail.com>
2013-02-03 10:05:21 +0100

    Consume abottle of mate via the rest interface.

---
+++ mate.application/src/main/java/de/atomfrede/mate/application/rest/consume/ComsumptionController.java
@@ -0,0 +1,55 @@
+package de.atomfrede.mate.application.rest.consume;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import de.atomfrede.mate.application.rest.common.LoginFailedException;
+import de.atomfrede.mate.application.rest.common.TokenUtil;
+import de.atomfrede.mate.domain.entities.user.User;
+import de.atomfrede.mate.service.consumption.ConsumptionService;
+
+@Controller
+public class ComsumptionController {
+
+	@Autowired
+	TokenUtil tokenUtil;
+	
+	@Autowired
+	ConsumptionService consumptionService;
+	
+	@ResponseBody
+	@RequestMapping(value = "/consume", method = RequestMethod.PUT)
+	public User getRefreshed(@RequestHeader("Authorization") String basicAuth,
+			HttpServletRequest request, HttpServletResponse response){
+		
+		try {
+			HashMap<String, Object> loginResult = tokenUtil
+					.generateAccessToken(basicAuth);
+
+			User user = (User) loginResult.get(TokenUtil.USER_KEY);
+			if (user != null) {
+				consumptionService.consumeBottle(user);
+				return user.toJsonTransferable();
+			}
+			throw new LoginFailedException();
+
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LoginFailedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
---
+++ mate.domain/src/main/java/de/atomfrede/mate/domain/entities/preferences/Preferences.java
@@ -0,0 +1,8 @@
+package de.atomfrede.mate.domain.entities.preferences;
+
+public class Preferences {
+	
+	protected String notificationmail;
+	protected String smtpserver;
+
+}
