70f683f4392d7ba052f25805c417db48f5bfe219
Frederik Hahne <frederik.hahne@googlemail.com>
2013-02-10 12:41:17 +0100

    Displaying and updating current account value and consume dbottles in my account panel.

--- mate.application/src/main/java/de/atomfrede/mate/application/wicket/account/MyAccountPanel.java
+++ mate.application/src/main/java/de/atomfrede/mate/application/wicket/account/MyAccountPanel.java
@@ -1,13 +1,49 @@
 package de.atomfrede.mate.application.wicket.account;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
+
+import de.atomfrede.mate.application.wicket.model.AbstractEntityModel;
+import de.atomfrede.mate.domain.entities.user.User;
 
 public class MyAccountPanel extends Panel {
 
-	public MyAccountPanel(String id,IModel<?> model) {
+	Label myAccountValue, myAccountBottles;
+
+	AbstractEntityModel<User> model;
+
+	public MyAccountPanel(String id, AbstractEntityModel<User> model) {
 		super(id, model);
-		// TODO Auto-generated constructor stub
+		this.model = model;
+		updateLabels();
+	}
+
+	@Override
+	public void onBeforeRender(){
+		super.onBeforeRender();
+		updateLabels();
+	}
+	
+	private void updateLabels() {
+		double accountValue = model.getObject().getAccount().getValue();
+		int consumedBottles = model.getObject().getConsumptions().size();
+
+		if (myAccountValue != null) {
+			remove(myAccountValue);
+		}
+		if (myAccountBottles != null) {
+			remove(myAccountBottles);
+		}
+
+		myAccountValue = new Label("my-account-value", "Kontostand: "
+				+ accountValue + "€");
+		myAccountBottles = new Label("my-account-bottles",
+				"Konsumierte Flaschen: " + consumedBottles);
+
+		add(myAccountValue);
+		add(myAccountBottles);
 	}
 
 }
