d3c0239826754cfdb82a315a71921a2ef6f920ac
atomfrede <atomfrede@gmail.com>
2014-07-11 21:47:11 +0200

    more tests.

---
+++ mate.domain/src/test/java/de/atomfrede/mate/domain/entities/account/AccountTest.java
@@ -0,0 +1,17 @@
+package de.atomfrede.mate.domain.entities.account;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Test
+	public void assertThatValueIsIncreased() {
+		Account a = new Account();
+		double before = a.getValue();
+		a.increaseBy(10.0);
+		
+		assertThat(a.getValue()).isEqualTo(before + 10.0);
+	}
+}
