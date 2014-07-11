df2da24fdc11dde26f49c87fb74e9cd38af3bb3c
atomfrede <atomfrede@gmail.com>
2014-07-11 20:08:45 +0200

    Tests and mailservice.

--- mate.domain/src/test/java/de/atomfrede/mate/domain/dao/user/UserDaoTest.java
+++ mate.domain/src/test/java/de/atomfrede/mate/domain/dao/user/UserDaoTest.java
@@ -1,6 +1,6 @@
 package de.atomfrede.mate.domain.dao.user;
 
-import static org.fest.assertions.api.Assertions.*;
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -11,11 +11,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.AssertThrows;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import de.atomfrede.mate.domain.dao.user.UserDao;
 import de.atomfrede.mate.domain.entities.account.Account;
 import de.atomfrede.mate.domain.entities.user.User;
 
@@ -47,6 +45,17 @@ public class UserDaoTest {
 	}
 
 	@Test
+	public void assertThatNotPersitedUserIsNew() {
+		User testUser2 = new User();
+		assertThat(testUser2.isNew()).isTrue();
+	}
+	
+	@Test
+	public void assertThatPersistedUserIsNotNew() {
+		assertThat(testUser.isNew()).isFalse();
+	}
+	
+	@Test
 	public void findAllTest() {
 		List<User> allUsers = userDao.findAll();
 		assertNotNull(allUsers);
@@ -61,12 +70,76 @@ public class UserDaoTest {
 		User found = userDao.findById(id);
 		assertThat(found).isNull();
 	}
-	
+
 	@Test
 	public void assertThatCanBeFoundByUsername() {
 		User found = userDao.getByUserName("mmuster");
 		assertThat(found).isNotNull();
 		assertThat(found.getUsername()).isEqualTo("mmuster");
 	}
+
+	@Test
+	public void assertThatListReturnsCorrectData() {
+		// Second TestUser
+		User testUser2 = new User();
+		testUser2.setEmail("max@muster.org");
+		testUser2.setFirstname("Max");
+		testUser2.setLastname("Muster2");
+		testUser2.setUsername("mmuster2");
+		testUser2.setAccount(new Account());
+		userDao.persist(testUser2);
+
+		List<User> retrieved = userDao.list(0, 1);
+
+		assertThat(retrieved).isNotEmpty();
+		assertThat(retrieved.size()).isEqualTo(1);
+		assertThat(retrieved.get(0).getUsername()).isEqualTo("mmuster");
+		
+		userDao.remove(testUser2);
+	}
+
+	@Test
+	public void assertThatListCanBeOrderedAsc() {
+		// Second TestUser
+		User testUser2 = new User();
+		testUser2.setEmail("max@muster.org");
+		testUser2.setFirstname("Max");
+		testUser2.setLastname("Muster2");
+		testUser2.setUsername("amuster");
+		testUser2.setAccount(new Account());
+		userDao.persist(testUser2);
+
+		List<User> retrieved = userDao.list(0, 15, "username", false);
+
+		assertThat(retrieved).isNotEmpty();
+		assertThat(retrieved.get(0).getUsername()).isEqualTo("amuster");
+		
+		userDao.remove(testUser2);
+	}
+
+	@Test
+	public void assertThatListCanBeOrderedDesc() {
+		// Second TestUser
+		User testUser2 = new User();
+		testUser2.setEmail("max@muster.org");
+		testUser2.setFirstname("Max");
+		testUser2.setLastname("Muster2");
+		testUser2.setUsername("amuster");
+		testUser2.setAccount(new Account());
+		userDao.persist(testUser2);
+
+		List<User> retrieved = userDao.list(0, 15, "username", true);
+
+		assertThat(retrieved).isNotEmpty();
+		assertThat(retrieved.get(0).getUsername()).isEqualTo("mmuster");
+		
+		userDao.remove(testUser2);
+	}
+	
+	@Test
+	public void assertThatCountReturnsCorrectCount(){
+		assertThat(userDao.count()).isEqualTo(1L);
+	}
+	
 
 }
--- mate.service/src/main/java/de/atomfrede/mate/service/email/MailServiceImpl.java
+++ mate.service/src/main/java/de/atomfrede/mate/service/email/MailServiceImpl.java
@@ -2,6 +2,7 @@ package de.atomfrede.mate.service.email;
 
 import java.util.List;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
@@ -20,27 +21,36 @@ public class MailServiceImpl implements MailService {
 	@Autowired
 	UserService userService;
 
+	private Email createSimpleMail() throws ConfigurationException, EmailException {
+		MailConfig config = new MailConfig();
+		Email email = new SimpleEmail();
+		email.setHostName(config.host());
+		email.setSmtpPort(config.port());
+		email.setAuthenticator(new DefaultAuthenticator(config.user(), config.password()));
+		email.setSSLOnConnect(config.ssl());
+		email.setFrom(config.from());
+		
+		return email;
+	}
+	
 	@Override
 	public void sendNoMatesMail() {
 		List<User> allUsers = userService.findAll();
 		try {
-
-			Email email = new SimpleEmail();
-			// email.setHostName("smtp.googlemail.com");
-//			email.setSmtpPort(465);
-//			email.setAuthenticator(new DefaultAuthenticator("username",
-//					"password"));
-			email.setSSLOnConnect(true);
-			email.setFrom("supplies@matetracker.com");
-			email.setSubject("No Mates left!");
+			
+			Email email = createSimpleMail();
+			email.setSubject("No Mates left, oh Dear!");
 			email.setMsg("There are no mates left. We need supplies *now*!");
 
 			for (User user : allUsers) {
-				String mail = user.getEmail();
-				email.addTo(mail);
+				if(user.isMailNotification()) {
+					String mail = user.getEmail();
+					email.addTo(mail);
+				}
+				
 			}
 			email.send();
-		} catch (EmailException emailEx) {
+		} catch (EmailException | ConfigurationException emailEx) {
 
 		}
 
@@ -51,22 +61,19 @@ public class MailServiceImpl implements MailService {
 		List<User> allUsers = userService.findAll();
 		try {
 
-			Email email = new SimpleEmail();
-			// email.setHostName("smtp.googlemail.com");
-//			email.setSmtpPort(465);
-//			email.setAuthenticator(new DefaultAuthenticator("username",
-//					"password"));
-			email.setSSLOnConnect(true);
-			email.setFrom("supplies@matetracker.com");
+			Email email = createSimpleMail();
 			email.setSubject("There are "+availableMates+" left.");
-			email.setMsg("Only "+availableMates+" left. You should resupply soon.");
+			email.setMsg("We are running low on Mate. Only "+availableMates+" left. You should resupply soon.");
 
 			for (User user : allUsers) {
-				String mail = user.getEmail();
-				email.addTo(mail);
+				if(user.isMailNotification()) {
+					String mail = user.getEmail();
+					email.addTo(mail);
+				}
+				
 			}
 			email.send();
-		} catch (EmailException emailEx) {
+		} catch (EmailException | ConfigurationException emailEx) {
 
 		}
 
