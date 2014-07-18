2fe173772cd0a8575f3b07100bc6479a96aa1fdd
atomfrede <atomfrede@gmail.com>
2014-07-18 23:31:11 +0200

    RegisterPage render test.

---
+++ mate.application/src/test/java/de/atomfrede/mate/application/wicket/register/RegisterPageRenderTest.java
@@ -0,0 +1,23 @@
+package de.atomfrede.mate.application.wicket.register;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.atomfrede.mate.application.wicket.WicketPageRenderTest;
+import de.atomfrede.mate.application.wicket.homepage.Homepage;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:WEB-INF/applicationContext.xml" })
+public class RegisterPageRenderTest extends WicketPageRenderTest {
+
+	@Test
+	@DirtiesContext
+	public void assertThatRegisterPageCanBeRendered() {
+		tester.startPage(RegisterPage.class);
+		tester.assertRenderedPage(RegisterPage.class);
+	}
+	
+}
