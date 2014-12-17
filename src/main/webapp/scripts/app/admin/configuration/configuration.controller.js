0c55d7d19c4265efb5695cce47839a1712076edb
atomfrede <atomfrede@gmail.com>
2014-12-17 18:31:51 +0100

    Updated jhipster release.

---
+++ src/main/webapp/scripts/app/admin/configuration/configuration.controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('matetrackerApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('configuration', {
+                parent: 'admin',
+                url: '/configuration',
+                data: {
+                    roles: ['ROLE_ADMIN']
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/admin/configuration/configuration.html',
+                        controller: 'ConfigurationController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('configuration');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    })
+    .controller('ConfigurationController', function ($scope, ConfigurationService) {
+        ConfigurationService.get().then(function (configuration) {
+            $scope.configuration = configuration;
+        });
+    });
